5b4ced0af07f3bc2b95749b39cec02f39961e22b
faisal Az <faisalabdulaziz761@gmail.com>
2025-10-08 20:16:56 +0700

    perubahan 11

--- src/pages/AdminLogin.js
+++ src/pages/AdminLogin.js
@@ -24,7 +24,7 @@ const AdminLogin = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("https://buku-tamu-be.vercel.app/api/users/login", {
+      const response = await fetch("https://buku-tamu-be.vercel.app/api/users/Login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
